2c2eb0bb601ee2d2eb1d1a101482f99b54bca08a
Invizabel <46697708+Invizabel@users.noreply.github.com>
2022-03-16 16:42:09 -0500

    Add files via upload

--- Volor Savanna Adventures/Volor Savanna Adventures.py
+++ Volor Savanna Adventures/Volor Savanna Adventures.py
@@ -16,6 +16,10 @@ screen_size = pygame.display.Info()
 my_screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
 my_screen.fill("yellow")
 
+#global variables
+tree_x = []
+tree_y = []
+
 def entity_diamond():
     return "blue"
 
@@ -28,7 +32,28 @@ def entity_lapis_lazuli():
 def entity_redstone():
     return "red"
 
+def generate_terrain():
+    #seed number is for development only
+    #trees
+    original_seed = 11182001
+    seed_x = random.seed(original_seed)
+    seed_x = random.random()
+    seed_x = seed_x * 10
+    seed_x = math.ceil(seed_x)
+    mod_seed = "123"[::-1]
+    mod_seed = int(mod_seed)
+    seed_y = random.seed(mod_seed)
+    seed_y = random.random()
+    seed_y = seed_y * 10
+    seed_y = math.ceil(seed_y)
+    
+    for i in range(0,math.ceil(screen_size.current_h / 5),50):
+        tree_x.append(i * seed_x)
+        tree_y.append(i * seed_y)
+    
 def one_player():
+    generate_terrain()
+    
     #inventory
     break_entity = False
     entity_list = []
@@ -49,6 +74,8 @@ def one_player():
     #load images
     lion_image_1 = pygame.image.load("Images/The Mighty Lion 1.png")
     lion_image_1 = pygame.transform.scale(lion_image_1,(100, 100))
+    tree_image = pygame.image.load("Images/tree.png")
+    tree_image = pygame.transform.scale(tree_image,(100, 100))
 
     #player stats
     player_x = screen_size.current_w / 2
@@ -190,7 +217,19 @@ def one_player():
         if player_health > 0:
             my_screen.blit(lion_image_1,(player_x, player_y))
             my_screen.blit(player_health_font, (100,(screen_size.current_h) - 100))
-            
+
+        #block break detection
+        '''if break_entity == True:
+            break_entity = False
+
+            for i in range(len(tree_x)):
+                #print(i, len(tree_x))
+                if i < len(tree_x):
+                    if player_x <= tree_x[i] + 100 and player_x >= tree_x[i] - 100:
+                        if player_y <= tree_y[i] + 100 and player_y >= tree_y[i] - 100:
+                            tree_x.pop(i)
+                            tree_y.pop(i)'''
+                    
         #place entity
         if place_entity == True:
             my_boolean = False
@@ -215,6 +254,7 @@ def one_player():
         #break entity
         if break_entity == True:
             break_entity = False
+
             for i in range(len(super_entity_list_x)):
                 if i < len(super_entity_list_x):
                     if super_entity_list_x[i] <= player_x + 100 and super_entity_list_x[i] >= player_x - 100:
@@ -223,10 +263,24 @@ def one_player():
                             super_entity_list_y.pop(i)
                             entity_list.pop(i)
 
+            for i in range(len(tree_x)):
+                if i < len(tree_x):
+                    if player_x <= tree_x[i] + 100 and player_x >= tree_x[i] - 100:
+                        if player_y <= tree_y[i] + 100 and player_y >= tree_y[i] - 100:
+                            tree_x.pop(i)
+                            tree_y.pop(i)
+
+            
+
         #render entities
         if super_entity == True and load_game == False:
             for i in range(len(super_entity_list_x)):
                 pygame.draw.rect(my_screen, entity_list[i], pygame.Rect(super_entity_list_x[i], super_entity_list_y[i], 100, 100))
+
+        #render trees
+        if load_game == False:
+            for i in range(len(tree_x)):
+                my_screen.blit(tree_image,(tree_x[i], tree_y[i]))
 
         if load_game == True:
             load_game = False
