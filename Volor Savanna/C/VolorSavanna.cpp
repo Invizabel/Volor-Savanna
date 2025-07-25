c9955069b804db4105d05009798718b02ca45ac6
Invizabel <46697708+Invizabel@users.noreply.github.com>
2025-07-24 23:49:16 -0500

    Add files via upload

--- Volor Savanna/C/VolorSavanna.cpp
+++ Volor Savanna/C/VolorSavanna.cpp
@@ -7,9 +7,31 @@ extern "C"
 
 int main()
 {
-    cout << name_prompt;
-    cin >> name;
-    cout << character_prompt  << endl;
-    cin >> character;
+    while (true)
+    {
+        cout << name_prompt;
+        cin >> name;
+        cout << character_prompt;
+        cin >> character;
+        if (character == "e")
+        {
+            return 0;
+        }
+
+        else
+        {
+            while (true)
+            {
+                cout << VolorSavannaGame();
+                cin >> choice;
+                if (level == 0)
+                {
+                    break;
+                }
+            }
+
+        }
+    }
+    
     return 0;
 }
